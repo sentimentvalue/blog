abb5d1b6988fc2db21a4879043e542b521c033b2
sentimentvalue <rkcldhkrkawjd@gmail.com>
2024-11-30 13:37:26 +0900

    owner img changed

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ const users = [
     username: "withree",
     company: "withree",
     position: "CEO",
-    img: "img/user/profile-licat.png",
+    img: "img/user/profile-zeezee.png",
   },
 ];
 
